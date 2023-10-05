735b07c1a127b02bb047fcf6f661076460b33965
saikrishna823 <20131a05f4@gvpce.ac.in>
2023-10-06 00:01:18 +0530

    second-commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import "../styles/Home.css"
-import profile from "../assets/profile.jpg"
+import profile from "../assets/profile.png"
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import {Link} from "react-router-dom";
-function Home(props) {
+function Home(p) {
     const resumeUrl="portfolio\src\assets\Sai_Krishna_Reddy.pdf";
     const handleDownload=()=>{
         const link=document.createElement('a');
